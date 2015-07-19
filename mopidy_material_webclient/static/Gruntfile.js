49e23ba7e2cd655ffa14aa577d8f0f5ae711d1d9
Mat Gallacher <mat.gallacher@gmail.com>
2015-07-19 11:58:20 +0100

    - Reworked searching from the library - Playing a track now inserts it into the current queue then plays it - Clear queue function - Lots of cleanup and linting of scripts - Added missing files to the pypi distribution

---
+++ mopidy_material_webclient/static/Gruntfile.js
@@ -0,0 +1,58 @@
+/*global module:false*/
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy: {
+            deoendencies: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'bower_components',
+                        src: ['angular/*.min.js',
+                            'angular-animate/*.min.js',
+                            'angular-aria/*.min.js',
+                            'angular-route/*.min.js',
+                            'angular-material/*.min.js'],
+                        dest: 'scripts/',
+                        flatten: true
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/angular-material',
+                        src: '*.min.css',
+                        dest: 'css/'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            scripts: {
+                files: {
+                    'scripts/app.min.js': ['src/*.js'],
+                }
+            }
+        },
+        less: {
+            'css/app.min.css': 'src/app.less'    
+        },
+        jshint: {
+            all: ['gruntfile.js', 'src/*.js']
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['copy', 'uglify', 'less']);
+
+};
